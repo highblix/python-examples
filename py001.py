32a5ebdea02993c27ae3cd948ca0294d7d5c2ee4
HighBlix <54054620+highblix@users.noreply.github.com>
2020-12-02 00:10:16 +0530

    Add files via upload

---
+++ py001.py
@@ -0,0 +1,12 @@
+#Python Program to Add Two Numbers
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+total = num1 + num2
+
+# Display the total sum of two numbers
+print('The sum of {0} and {1} is {2}'.format(num1, num2, total))
